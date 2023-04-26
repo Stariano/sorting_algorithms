8f979667924117d9d7db091c4c93327cb5e9a07b
Steve <evilco30@gmail.com>
2023-04-26 07:29:47 -0700

    Fixing some cases

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,24 @@
 void selection_sort(int *array, size_t size)
 {
 	int temp;
-	size_t itr_min, itr;
+	size_t itr_min, itr, minm;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (itr_min = 0; itr_min < size; itr_min++)
 	{
+		minm = itr_min;
 		for (itr = itr_min + 1; itr < size; itr++)
 		{
-			if (array[itr_min] > array[itr])
+			if (array[minm] > array[itr])
+				minm = itr;
+
+			if (minm != itr_min)
 			{
-				temp = array[itr_min];
-				array[itr_min] = array[itr];
-				array[itr] = temp;
+				temp = array[minm];
+				array[minm] = array[itr_min];
+				array[itr_min] = temp;
 			}
 		}
 		print_array(array, size);
